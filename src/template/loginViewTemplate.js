46c718d9c038568c90b3a5d9efd1b6abf63e9e90
aanufriev <anufriev.artem.mail@gmail.com>
2020-10-03 14:31:42 +0300

    Removed return from renderLoginView

--- src/template/loginViewTemplate.js
+++ src/template/loginViewTemplate.js
@@ -1,17 +1,15 @@
-export const renderLoginView = () => {
-    return window.Handlebars.compile(`
-        <div class="login_page">
-            <div class="col-container">
-                <img src="assets/img/logo.png" class="log_image">
-                <input type="email" class="input_text" placeholder="Логин:">
-                <input type="password" class="input_text" placeholder="Пароль:">
-                <div class="signup">
-                    <a href="" class="link">Зарегистрироваться</a>
-                </div>
-                <div>
-                    <button class="login">Войти</button>
-                </div>
+export const renderLoginView = () => window.Handlebars.compile(`
+    <div class="login_page">
+        <div class="col-container">
+            <img src="assets/img/logo.png" class="log_image">
+            <input type="email" class="input_text" placeholder="Логин:">
+            <input type="password" class="input_text" placeholder="Пароль:">
+            <div class="signup">
+                <a href="" class="link">Зарегистрироваться</a>
             </div>
-        </div>`
-    )
-}
+            <div>
+                <button class="login">Войти</button>
+            </div>
+        </div>
+    </div>`
+)
