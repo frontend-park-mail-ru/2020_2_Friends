51d05ebea032641540c13ceba5fa0e4ce333ee94
Scotfarel <scotfarel@gmail.com>
2020-10-06 15:18:06 +0300

    tested store templating

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-// import { LoginController } from './controller/loginController.js';
 import { StoreController } from './controller/storeController.js';
-// import { ProfileController } from './controller/profileController.js';
 
 const root = document.getElementById('root');
 
-// const loginController = new LoginController(root);
-// loginController.view.render();
 const storeController = new StoreController(root);
 storeController.view.render();
-// const profileController = new ProfileController(root);
-// profileController.view.render();
