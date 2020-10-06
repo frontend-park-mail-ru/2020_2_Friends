e2420db3965fe1d0c4c7a62d10569efab858ebeb
aanufriev <anufriev.artem.mail@gmail.com>
2020-10-06 11:58:56 +0300

    Added bind to redirect

--- src/utils/router.js
+++ src/utils/router.js
@@ -2,6 +2,7 @@ export class Router {
     constructor (root) {
         this.root = root;
         this.routes = new Map();
+        this.redirect = this.redirect.bind(this)
     }
 
     setRoute (path, controller) {
