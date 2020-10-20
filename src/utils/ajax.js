6dcc4c68da888e6c3301d5f70cc272cd644501fe
Scotfarel <scotfarel@gmail.com>
2020-10-20 11:53:19 +0300

    Rework to oneliner

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -1,25 +1,22 @@
 export const ajaxGetUsingFetch = async (ajaxArgs) => {
-    const response = await fetch(ajaxArgs.url, {
+    return await fetch(ajaxArgs.url, {
         method: 'GET',
         credentials: 'include'
     });
-    return response;
 }
 
 export const ajaxPostUsingFetch = async (ajaxArgs) => {
-    const response = await fetch(ajaxArgs.url, {
+    return await fetch(ajaxArgs.url, {
         method: 'POST',
         credentials: 'include',
         body: JSON.stringify(ajaxArgs.body)
     });
-    return response;
 }
 
 export const ajaxPutUsingFetch = async (ajaxArgs) => {
-    const response = await fetch(ajaxArgs.url, {
+    return await fetch(ajaxArgs.url, {
         method: 'PUT',
         credentials: 'include',
         body: JSON.stringify(ajaxArgs.body)
     })
-    return response;
 }
