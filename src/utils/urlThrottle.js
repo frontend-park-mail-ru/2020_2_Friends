418e9a577130cda6c0d469a4998e848f664ad9c6
Scotfarel <scotfarel@gmail.com>
2020-10-21 00:43:20 +0300

    Removed comments

--- src/utils/urlThrottle.js
+++ src/utils/urlThrottle.js
@@ -1,5 +1,4 @@
 export const makeAvatarUrl = (avatarName) => {
-    // Я блять должен это из конфига сервера получать сука а не руками писать нахуй бекендеры вы ебаные или создайте блять енвайромент переменную
     const schema = 'http://';
     const host = 'localhost';
     const staticPort = ':9001';
