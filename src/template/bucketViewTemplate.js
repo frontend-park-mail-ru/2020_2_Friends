90cbaf7ed9ee0b662fe8b4459242346c809801b8
naysudes <syrbulova@gmail.com>
2020-11-16 21:16:00 +0300

    выравнивание исправлено

--- src/template/bucketViewTemplate.js
+++ src/template/bucketViewTemplate.js
@@ -17,10 +17,10 @@ export const renderBucketView = () => window.Handlebars.compile(`
                     <div class="bucket-item__name">{{this.food_name}}</div>
                     <div class="bucket-item__descr">{{this.food_price}}</div>
                 </div>
-                    <div class="bucket-item__row">
-                        <input class="bucket-item__quantity" type="number" value="1">
-                        <button class="bucket-item__delete js-delete-item"></button>
-                    </div>
+                <div class="bucket-item__row">
+                    <input class="bucket-item__quantity" type="number" value="1">
+                    <button class="bucket-item__delete js-delete-item"></button>
+                </div>
             </div>
             {{/each}}
         </div>
@@ -29,10 +29,10 @@ export const renderBucketView = () => window.Handlebars.compile(`
             <div class="bucket-total__header">Итого:</div>
             <div class="bucket-total__store-name">Выберете адрес:</div>
             <select id="js-address">
-            {{#each address}}
-            <option value="{{this}}" class="status__option">{{this}}</option>
-            {{/each}}
-        </select>
+                {{#each address}}
+                <option value="{{this}}" class="status__option">{{this}}</option>
+                {{/each}}
+            </select>
             <div class="bucket-total__store-name">{{this.store_name}}</div>
             <div class="bucket-total__sum">Сумма заказа:{{total}}</div>
             <div class="proceed-button bucket-total__order-button js-make-order">Заказать</div>
