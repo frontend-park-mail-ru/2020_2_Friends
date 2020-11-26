2710f0db4c5087ec1a5fc33e537ca202d6ece5c5
naysudes <syrbulova@gmail.com>
2020-11-26 21:07:12 +0300

    делаем чатик

--- src/template/chatViewTemplate.js
+++ src/template/chatViewTemplate.js
@@ -19,21 +19,33 @@ export const chatView = () => window.Handlebars.compile(`
     </div>
     <div class="chat">
         <div class="chat-header">
-            <div class="chat-header-nickname">@qweasd</div>
+            <div class="chat-header-nickname">Чат с магазином</div>
         </div>
         <div class="chat-messages">
             <div class="chat-message__from-user">
-                <div class="chat-message-text__from-user">hi</div>
+                <div class="chat-message-text__from-user">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
             </div>
             <div class="chat-message__from-user">
                 <div class="chat-message-text__from-user">hi</div>
             </div>
+            <div class="chat-message__to-user">
+                <div class="chat-message-text__to-user">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
+            </div>
             <div class="chat-message__from-user">
                 <div class="chat-message-text__from-user">hi</div>
+            </div>
+                        <div class="chat-message__to-user">
+                <div class="chat-message-text__to-user">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
+            </div>
+                        <div class="chat-message__to-user">
+                <div class="chat-message-text__to-user">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
+            </div>
+                        <div class="chat-message__to-user">
+                <div class="chat-message-text__to-user">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
             </div>
         </div>
         <div class="chat-new-message"><input class="chat-new-message-input input" type="text"
-                placeholder="Напишите сообщение..." value=""><button class="button button--secondary">Отправить</button></div>
+                placeholder="Напишите сообщение..." value=""><button class="proceed-button">Отправить</button></div>
     </div>
 </div>
 `);
