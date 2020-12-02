f9710b9ec4675c76635b682bc5dc8934c9d14e85
Scotfarel <scotfarel@gmail.com>
2020-12-02 22:43:30 +0300

    Added src for ymaps

--- src/index.js
+++ src/index.js
@@ -17,6 +17,35 @@ import { PartnerStoreController } from './controller/partnerStoreController.js';
 import { ChatController } from './controller/chatController.js';
 import { Router } from './utils/router.js';
 
+export default {
+    load (src = '//api-maps.yandex.ru/2.1/?lang=en_RU') {
+        const getNsParamValue = () => {
+            const results = src.match(/[\\?&]ns=([^&#]*)/);
+            return results === null
+                ? ''
+                : decodeURIComponent(results[1].replace(/\+/g, ' '));
+        };
+        if (!this.promise) {
+            this.promise = new Promise((resolve, reject) => {
+                const scriptElement = document.createElement('script');
+                scriptElement.onload = resolve;
+                scriptElement.onerror = reject;
+                scriptElement.type = 'text/javascript';
+                scriptElement.src = src;
+                document.body.appendChild(scriptElement);
+            }).then(() => {
+                const ns = getNsParamValue();
+                if (ns && ns !== 'ymaps') {
+                    // eslint-disable-next-line no-eval
+                    (1, eval)(`var ymaps = ${ns};`);
+                }
+                return new Promise(resolve => ymaps.ready(resolve));
+            });
+        }
+        return this.promise;
+    }
+};
+
 const serviceLoad = () => {
     const root = document.getElementById('root');
     const header = document.getElementById('header');
